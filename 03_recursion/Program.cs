b91b30460999b18fc0f6d7d959e4108f1288c846
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2023-08-09 11:22:32 -0300

    up 3 exerise

---
+++ 03_recursion/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("03 Recursion!");
+
+#region Coutdown
+Console.WriteLine("======COUTDOWN=======");
+Countdown(5);
+static void Countdown(int i)
+{
+    Console.WriteLine(i);
+
+    // base case
+    if (i <= 0) return;
+
+    // recursive case
+    Countdown(i - 1);
+}
+#endregion
+
+#region Greet 
+Console.WriteLine("======GREET=======");
+Greet("adit");
+
+static void Greet(string name)
+{
+    Console.WriteLine($"hello, {name}!");
+    Greet2(name);
+    Console.WriteLine("getting ready to say bye...");
+    Bye();
+}
+
+static void Greet2(string name)
+{
+    Console.WriteLine($"how are you, {name}?");
+}
+
+static void Bye()
+{
+    Console.WriteLine("ok bye!");
+}
+
+#endregion
+
+#region Factorial
+Console.WriteLine("======FACTORIAL=======");
+
+Console.WriteLine(Fact(5));
+static int Fact(int x)
+{
+    if (x <= 1) return 1;
+
+    return x * Fact(x - 1);
+}
+#endregion
